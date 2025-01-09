63c7a0f08d32fc88c273e2e3163200ec663354c8
willardcabrera <wxefwpdycfcah8r1486knmjk9tdj6h@bots.bitbucket.org>
2025-01-08 20:32:12 -0500

    Initial Commit

---
+++ notesTests/notesTests.swift
@@ -0,0 +1,17 @@
+//
+//  notesTests.swift
+//  notesTests
+//
+//  Created by Willard Cabrera on 8/01/25.
+//
+
+import Testing
+@testable import notes
+
+struct notesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
